b660f4ce61a7cbcdaeb70cca5d0c9a1756c59a5e
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-18 14:35:08 -0500

    containers: Create Phonograms container

---
+++ src/containers/Phonograms.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+const Phonograms = () => {
+  return (
+    <div className="view">
+      <section className="head">
+        <Typography variant="h1" gutterBottom>Fonogramas</Typography>
+      </section>
+    </div>
+  );
+}
+
+export default Phonograms;
