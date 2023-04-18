d92af9d38d7a8df96990465760d374ec7dc0f1d4
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-18 14:35:37 -0500

    containers: Create MusicalWorks container

---
+++ src/containers/MusicalWorks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+const MusicalWorks = () => {
+  return (
+    <div className="view">
+      <section className="head">
+        <Typography variant="h1" gutterBottom>Obras Musicales</Typography>
+      </section>
+    </div>
+  );
+}
+
+export default MusicalWorks;
