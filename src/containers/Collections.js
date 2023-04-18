381e1fa9e0b0c06a4fc04da0e12c2f2d0afd7652
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-18 14:35:56 -0500

    containers: Create Collections container

---
+++ src/containers/Collections.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+const Collections = () => {
+  return (
+    <div className="view">
+      <section className="head">
+        <Typography variant="h1" gutterBottom>Recaudaciones</Typography>
+      </section>
+    </div>
+  );
+}
+
+export default Collections;
