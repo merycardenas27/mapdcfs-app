26c4ba6b64e32bd69ae0b293b79f8adf2bfd9194
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-18 23:16:30 -0500

    app: Add custom theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
 
 import './App.css';
+import theme from './styles/theme';
+
 import Collections from './containers/Collections';
 import Home from './containers/Home';
 import Interpreters from './containers/Interpreters';
@@ -15,22 +18,24 @@ const queryClient = new QueryClient();
 function App() {
   return (
     // Provide the client to App
-    <QueryClientProvider client={queryClient}>
-      <div className="app">
-        <div className="wrapper">
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='fonogramas/' element={<Phonograms />} />
-              <Route path='interpretes/' element={<Interpreters />} />
-              <Route path='obras-musicales/' element={<MusicalWorks />} />
-              <Route path='recaudaciones/' element={<Collections />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      </div>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <div className="app">
+            <div className="wrapper">
+              <BrowserRouter>
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='fonogramas/' element={<Phonograms />} />
+                  <Route path='interpretes/' element={<Interpreters />} />
+                  <Route path='obras-musicales/' element={<MusicalWorks />} />
+                  <Route path='recaudaciones/' element={<Collections />} />
+                </Routes>
+              </BrowserRouter>
+            </div>
+          </div>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ThemeProvider>
   );
 }
 
