4b7eb5bde9df36c03a23139cead734d4d2a98155
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-19 17:41:05 -0500

    containers(Phonograms): Add table to see all phonograms

--- src/containers/Phonograms.js
+++ src/containers/Phonograms.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
 import Typography from '@mui/material/Typography';
 
+import { getPhonograms } from '../queries';
+
+import AlertError from '../components/CustomAlertError';
+import AlertInfo from '../components/CustomAlertInfo';
+import Loader from '../components/CustomLoader';
+import Table from '../components/CustomTable';
+
 const Phonograms = () => {
+  const {
+    data: phonograms,
+    error,
+    isError,
+    isLoading,
+  } =useQuery('phonograms', getPhonograms);
+
   return (
     <div className="view">
       <section className="head">
         <Typography variant="h1" gutterBottom>Fonogramas</Typography>
       </section>
+      { isLoading
+        ? <Loader />
+        : isError
+          ? <AlertError message={error.message} />
+          : <>
+              <section className="body">
+                {!phonograms.length
+                  ? <AlertInfo message="No hay fonogramas registrados" />
+                  : <Table
+                      columns={['ID', 'TITULO', 'DURACIÓN (seg)', 'F. DE CREACIÓN', 'GÉNERO', 'OBRA MUSICAL', 'PRODUCTORA', 'ACCIONES']}
+                      rows={phonograms}
+                    />
+                }
+              </section>
+            </>
+      }
     </div>
   );
 }
