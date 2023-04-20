bb1c542cd23f44fcc4135509762d029f8744903f
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-20 09:24:09 -0500

    containers(MusicalWorks): Add table to see all musical works

--- src/containers/MusicalWorks.js
+++ src/containers/MusicalWorks.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
 import Typography from '@mui/material/Typography';
 
+import { getMusicalWorks } from '../queries';
+
+import AlertError from '../components/CustomAlertError';
+import AlertInfo from '../components/CustomAlertInfo';
+import Loader from '../components/CustomLoader';
+import Table from '../components/CustomTable';
+
 const MusicalWorks = () => {
+  const {
+    data: items,
+    error,
+    isError,
+    isLoading,
+  } =useQuery('musicalWorks', getMusicalWorks);
+
   return (
     <div className="view">
       <section className="head">
         <Typography variant="h1" gutterBottom>Obras Musicales</Typography>
       </section>
+      {isLoading
+        ? <Loader />
+        : isError
+          ? <AlertError message={error.message} />
+          : <>
+              <section className="body">
+                {!items.length
+                  ? <AlertInfo message="No hay obras musicales registradas" />
+                  : <Table
+                      columns={['ID', 'TITULO', 'TIPO DE OBRA', 'ACCIONES']}
+                      rows={items}
+                    />
+                }
+              </section>
+            </>
+      }
     </div>
   );
 }
