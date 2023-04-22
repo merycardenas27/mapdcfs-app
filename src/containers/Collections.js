2826f1f349642b7989f6bb4664db5dab76101d8a
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-22 16:53:19 -0500

    containers(Collections): Add action to add a collection

--- src/containers/Collections.js
+++ src/containers/Collections.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 import { getCollections } from '../queries';
 
 import AlertError from '../components/CustomAlertError';
 import AlertInfo from '../components/CustomAlertInfo';
+import DialogToAdd from '../components/DialogToAddCollection';
 import Loader from '../components/CustomLoader';
 import Table from '../components/CustomTable';
 
 const Collections = () => {
+  const [openAddDialog, setOpenAddDialog] = useState(false);
+
   const {
     data: collections,
     error,
     isError,
     isLoading,
+    refetch,
   } =useQuery('collections', getCollections);
+
+  const handleOpenAddDialog = () => {
+    setOpenAddDialog(true);
+  };
+
+  const handleCloseAddDialog = () => {
+    setOpenAddDialog(false);
+  };
 
   return (
     <div className="view">
@@ -28,6 +44,13 @@ const Collections = () => {
         : isError
           ? <AlertError message={error.message} />
           : <>
+              <section className="actions">
+                <Tooltip title="Agregar">
+                  <IconButton aria-label="agregar" color="primary" onClick={handleOpenAddDialog}>
+                    <PersonAddIcon />
+                  </IconButton>
+                </Tooltip>
+              </section>
               <section className="body">
                 {!collections.length
                   ? <AlertInfo message="No hay obras musicales registradas" />
@@ -36,6 +59,13 @@ const Collections = () => {
                       rows={collections}
                     />
                 }
+              </section>
+              <section className="dialogs">
+                <DialogToAdd
+                  handleClose={handleCloseAddDialog}
+                  handleRefetch={refetch}
+                  open={openAddDialog}
+                />
               </section>
             </>
       }
