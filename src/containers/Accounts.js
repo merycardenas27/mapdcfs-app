f737f70e9f53d7fa6e1220795e0bc9c9f5876f45
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-23 16:53:26 -0500

    containers: Create Account container

---
+++ src/containers/Accounts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Typography from '@mui/material/Typography';
+
+const Accounts = () => {
+  return (
+    <div className="view">
+      <section className="head">
+        <Typography variant="h1" gutterBottom>Cuentas</Typography>
+      </section>
+    </div>
+  );
+}
+
+export default Accounts;
