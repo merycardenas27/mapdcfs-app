826d52ce4b458d467aec36d62683a7296962f252
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-23 19:37:10 -0500

    components: Create DialogToAddAccount component

---
+++ src/components/DialogToAddAccount.js
@@ -0,0 +1,148 @@
+import * as React from 'react';
+import { useMutation } from 'react-query';
+import { useForm } from 'react-hook-form';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+
+import { addAccount } from '../mutations';
+
+export default function DialogToAddAccount({
+  handleClose,
+  handleRefetch,
+  open,
+}) {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    reset,
+  } = useForm({
+    mode: 'onSubmit',
+    defaultValues: {
+      name: '',
+      lastname: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const { isLoading, mutate } = useMutation(addAccount, {
+    onSuccess: () => {
+      handleRefetch('accounts');
+      handleClose();
+      reset();
+    },
+  });
+
+  const onSubmit = async ({
+    name,
+    lastname,
+    email,
+    password,
+  }) => {
+    mutate({
+      nombre: name,
+      apellido: lastname,
+      correo: email,
+      contraseña: password,
+    });
+  };
+
+  const onCancel = () => {
+    handleClose();
+    reset();
+  };
+
+  return (
+    <Dialog className="form" onClose={handleClose} open={open}>
+      <DialogTitle className="form-title">Agregar nueva cuenta</DialogTitle>
+      <DialogContent className="form-content">
+         <Box>
+          <Grid container spacing={2}>
+            <Grid item md={6}>
+              <TextField
+                error={!!errors.name}
+                fullWidth
+                helperText={errors.name?.message}
+                id="name"
+                inputProps={{...register('name',
+                  {
+                    required: 'Es requerido',
+                    pattern: {
+                      value: /^[a-zA-Z\s]*$/,
+                      message: 'Sólo letras',
+                    }
+                  }
+                )}}
+                label="Nombre"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={6}>
+              <TextField
+                error={!!errors.lastname}
+                fullWidth
+                helperText={errors.lastname?.message}
+                id="lastname"
+                inputProps={{...register('lastname',
+                  {
+                    required: 'Es requerido',
+                    pattern: {
+                      value: /^[a-zA-Z\s]*$/,
+                      message: 'Sólo letras',
+                    }
+                  }
+                )}}
+                label="Apellido"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={12}>
+              <TextField
+                error={!!errors.email}
+                fullWidth
+                helperText={errors.email?.message}
+                id="email"
+                inputProps={{ ...register('email', {
+                  required: 'Es requerido',
+                  pattern: {
+                    value: /^[\w\-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                    message: 'Debe ser un correo válido',
+                  }
+                })}}
+                label="Correo"
+                required
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item md={12}>
+              <TextField
+                error={!!errors.password}
+                fullWidth
+                helperText={errors.password?.message}
+                id="password"
+                inputProps={{ ...register('password', { required: 'Es requerido' })}}
+                label="Contraseña"
+                required
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel}>Cancel</Button>
+        <Button disabled={isLoading} type="submit" variant="contained" onClick={handleSubmit(onSubmit)}>Registrar</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
