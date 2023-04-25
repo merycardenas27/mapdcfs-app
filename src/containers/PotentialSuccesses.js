7a4f5cb5316b6d00550fd6d57b28187494d829ce
merycardenas27 <mery.cardenas.perez@gmail.com>
2023-04-25 15:32:39 -0500

    containers: Create PotentialSuccesses container

---
+++ src/containers/PotentialSuccesses.js
@@ -0,0 +1,112 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+
+import Button from '@mui/material/Button';
+import FormHelperText from '@mui/material/FormHelperText';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import TableViewIcon from '@mui/icons-material/TableView';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+
+import { Context } from '../store';
+import ConverToObjectArray from '../utils/convertToObjectArray';
+
+import Table from '../components/PotentialProccessTable';
+
+const filePattern = /([a-zA-Z0-9\s_\\.\-:()])+(.csv|.CSV|.xls|.XLS|.xlsx|.XLSX)$/;
+
+const PotentialSuccesses = () => {
+  const [state] = useContext(Context);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!state.isLogged) {
+      return navigate('/iniciar-sesion', {replace: true});
+    }
+  }, [state, navigate]);
+
+  const [sheets, setSheets] = useState(null);
+
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    reset,
+    watch,
+  } = useForm({
+    mode: 'onSubmit',
+    defaultValues: { file: '' },
+  });
+
+  const selectedFile = watch('file');
+
+  const onSubmit = async ({ file }) => {
+    const selectedSheets = await ConverToObjectArray(file[0]);
+    setSheets(selectedSheets);
+    reset();
+  };
+
+  return (
+    <div className="view">
+      <section className="head">
+        <Typography variant="h1" gutterBottom>Potenciales Éxitos</Typography>
+      </section>
+      <section className="actions">
+        <Grid container spacing={2}>
+          <Grid item md={6} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <label htmlFor="file">
+              <input
+                accept=".csv, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                id="file"
+                style={{ display: 'none' }}
+                type="file"
+                {...register('file', {
+                  required: 'Es requerido',
+                  validate: (value) => {
+                    const { name } = value[0];
+                    return filePattern.test(name) || 'Debe ser un archivo: .xlsx, .xls, .csv';
+                  }
+                })}
+              />
+              <Button
+                color="secondary"
+                component="span"
+                size="large"
+                startIcon={<UploadFileIcon />}
+                variant="contained"
+              >
+                Subir archivo
+              </Button>
+            </label>
+            <FormHelperText component="span" error={!!errors.file}   sx={{marginLeft: '8px'}}>
+              {errors.file
+                ? errors.file.message
+                : selectedFile && selectedFile[0]
+                  ? selectedFile[0].name
+                  : '*Seleccionar archivo .xlsx, .xls, .csv '}
+            </FormHelperText>
+          </Grid>
+          <Grid item md={6} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Button
+              disabled={!!errors.file}
+              onClick={handleSubmit(onSubmit)}
+              size="large"
+              startIcon={<TableViewIcon />}
+              type="submit"
+              variant="contained"
+            >
+              Generar predicción
+            </Button>
+          </Grid>
+        </Grid>
+      </section>
+      <section className="body">
+        {sheets?.length && <Table rows={sheets[0]} />}
+      </section>
+    </div>
+  );
+};
+
+export default PotentialSuccesses;
